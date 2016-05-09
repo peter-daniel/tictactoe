5409ede312f8454a85cba52fd2300335a397dcfa
peterdaniel-fewd <peter@Peters-MacBook-Pro-2.local>
2016-05-10 09:48:37 +1000

    attempting to use d.data method

--- js/script.js
+++ js/script.js
@@ -1,45 +1,29 @@
 // choose your token?
 
-// determine which player move it is - player TURN
 
-// determine the combination of what is a win for both players
-
-// use addClass to place
-
-// if X is placed use .find and .hasClass to log position to test
-
-// use set value and get value to determine outcome?
-
-
-
-
-
+var card = ''
 var turn = 0;
-var player = 'player1';
-console.log(turn);
-console.log(player);
-
-var boardArr = [1, 1, 1]
-   [1, 1, 1]
-   [1, 1, 1];
-
+var player = 'player 1';
+var gameEnd = 1;
 
 $('.column').on('click', function() {
-   if (!$(this).hasClass('x') && !$(this).hasClass('o')) {
+   if ($(this).text() == '') {
       if (turn % 2 !== 0) {
          $(this).addClass('x');
-         player = 'player2';
+         card = 'x'
+         player = 'player 2';
       } else {
          $(this).addClass('o');
-         player = 'player1';
+         player = 'player 1';
+        card = 'o'
       }
+      $(this).text(card);
       turn++;
       checkDraw()
       checkResult()
+      endTheGame()
       console.log(turn);
       console.log(player);
-   } else {
-      console.log('this space is taken');
    }
 });
 
@@ -50,58 +34,81 @@ function checkDraw() {
    }
 }
 
+function endTheGame() {
+   if (gameEnd === 0) {
+      $('.column').off('click');
+   }
+}
+
+function resetGame() {
+   $('.column').removecClass('x');
+   $('.column').removecClass('x');
+}
 
 function checkResult() {
-
    // horizontals
-   for (var x = 0; x <= 3; x++) {
-      for (var y = 0; y <= 3; y++) {
-         if ($('#ga .'+x+' .'+y+'').hasClass('x')) {
-            console.log('x wins');
-         } else if ($('#ga .'+x+' .'+y+'').hasClass('o')) {
-            console.log('O wins');
-         }
-      }
-   }
-
-   //verticals
-   for (var x = 0; x <= 3; x++) {
-      for (var y = 0; y <= 3; y++) {
-         if (
-            $('#ga .'+y+' .'+x+'').hasClass('x')) {
-            console.log('x wins');
-         } else if (
-            $('#ga .'+y+' .'+x+'').hasClass('o')) {
-            console.log('O wins');
-         }
-      }
-   }
-   // diagonals
    if (
-      $('#ga .1 .1').hasClass('x') &&
-      $('#ga .2 .2').hasClass('x') &&
-      $('#ga .3 .3').hasClass('x')) {
-      console.log('x wins');
+      $('.1').text() === card &&
+      $('.2').text() === card &&
+      $('.3').text() === card) {
+      $('.1, .2, .3').addClass('chosen');
+      gameEnd = 0;
+      console.log('the winner is ' + player);
    } else if (
-      $('#ga .1 .1').hasClass('o') &&
-      $('#ga .2 .2').hasClass('o') &&
-      $('#ga .3 .3').hasClass('o')) {
-      console.log('O wins');
+      $('.4').text() === card &&
+      $('.5').text() === card &&
+      $('.6').text() === card) {
+      $('.4, .5, .6').addClass('chosen');
+      gameEnd = 0;
+      console.log('the winner is ' + player);
+   } else if (
+      $('.7').text() === card &&
+      $('.8').text() === card &&
+      $('.9').text() === card) {
+      $('.7, .8, .9').addClass('chosen');
+      gameEnd = 0;
+      console.log('the winner is ' + player);
    }
+   // verticals
    if (
-      $('#ga .3 .1').hasClass('x') &&
-      $('#ga .2 .2').hasClass('x') &&
-      $('#ga .3 .1').hasClass('x')) {
-      console.log('x wins');
+      $('.1').text() === card &&
+      $('.4').text() === card &&
+      $('.7').text() === card) {
+      $('.1, .4, .7').addClass('chosen');
+      gameEnd = 0;
+      console.log('the winner is ' + player);
    } else if (
-      $('#ga .3 .1').hasClass('o') &&
-      $('#ga .2 .2').hasClass('o') &&
-      $('#ga .3 .1').hasClass('o')) {
-      console.log('O wins');
+      $('.2').text() === card &&
+      $('.5').text() === card &&
+      $('.8').text() === card) {
+      $('.2, .5, .8').addClass('chosen');
+      gameEnd = 0;
+      console.log('the winner is ' + player);
+   } else if (
+      $('.3').text() === card &&
+      $('.6').text() === card &&
+      $('.9').text() === card) {
+      $('.3, .6, .9').addClass('chosen');
+      gameEnd = 0;
+      console.log('the winner is ' + player);
    }
-};
-
-
+   //diagonals
+   if (
+      $('.1').text() === card &&
+      $('.5').text() === card &&
+      $('.9').text() === card) {
+      $('.1, .5, .9').addClass('chosen');
+      gameEnd = 0;
+      console.log('the winner is ' + player);
+   } else if (
+      $('.3').text() === card &&
+      $('.5').text() === card &&
+      $('.7').text() === card) {
+      $('.3, .5, .7').addClass('chosen');
+      gameEnd = 0;
+      console.log('the winner is ' + player);
+   }
+}
 
 
 
